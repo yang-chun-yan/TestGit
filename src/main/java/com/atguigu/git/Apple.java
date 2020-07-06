8863ff611001a502f316045c72031b5d1e484ad7
ycy <3240056286@qq.com>
2020-07-06 10:20:17 +0800

    My first commit in eclipse

---
+++ src/main/java/com/atguigu/git/Apple.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+/**
+* @author ycy
+* @version 1.0
+* @createDate 创建时间：2020/07/05 22:54:15
+* @explain(类说明):...
+*/
+public class Apple {
+
+}
---
+++ src/test/java/com/atguigu/test/GoodTest.java
@@ -0,0 +1,10 @@
+package com.atguigu.test;
+/**
+* @author ycy
+* @version 1.0
+* @createDate 创建时间：2020/07/05 22:56:36
+* @explain(类说明):...
+*/
+public class GoodTest {
+
+}
