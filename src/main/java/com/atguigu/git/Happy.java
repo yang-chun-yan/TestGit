a6925ee0011c4ad2f4e4fd193bddacd3ed7986e1
ycy <3240056286@qq.com>
2020-07-06 16:58:57 +0800

    test conflict

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -7,6 +7,6 @@ package com.atguigu.git;
 */
 public class Happy {
  public static void main(String[] args) {
-	System.out.println("I'm happy!!");
+	System.out.println("I'm happy!! right....");
 }
 }
